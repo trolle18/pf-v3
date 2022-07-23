320b0a7e3afc3ef6c0888d928b8b2f933bba00d5
Sofie Trolle <92292475+trolle18@users.noreply.github.com>
2022-07-23 14:31:27 +0200

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,32 @@
-import { AiOutlineGithub, AiFillCodepenCircle, AiFillLinkedin } from "react-icons/ai";
+import { AiOutlineGithub, AiOutlineCodepen } from "react-icons/ai";
+import { TiSocialLinkedin } from "react-icons/ti";
+// import { AiFillLinkedin } from "react-icons/ai";
+// import { FaLinkedinIn } from "react-icons/fa";
+// import { RiLinkedinFill } from "react-icons/ri";
+// import { FiGithub } from "react-icons/ri";
+
+import { FiCoffee, FiHeart } from "react-icons/fi"
+
 
 export default function Footer() {
     return (
         <>
             <section className="footer-cntr">
                 <div className="footer-top">
-                    <div className="footer-logo"> 
-                        <a href="#home">Sofie Trolle</a> 
+                    <div className="footer-l-col">
+                        <div className="footer-logo"> 
+                            <a href="#home">Sofie Trolle</a> 
+                        </div>
+                        <span>sofietrolle@hotmail.com</span>
                     </div>
                     <div className="footer-links">
-                        <a href="##"> <span><AiOutlineGithub/></span> </a> 
-                        <a href="##"> <span><AiFillCodepenCircle/></span> </a> 
-                        <a href="##"> <span><AiFillLinkedin/></span> </a>     
+                        <a href="##"> <AiOutlineGithub/> </a>
+                        <a href="##"> <AiOutlineCodepen/> </a>
+                        <a href="##"> <TiSocialLinkedin/> </a>
                     </div>
+                </div>
+                <div className="footer-btm">
+                    <span>Build with <FiHeart/> and <FiCoffee/> by Sofie Trolle </span>
                 </div>
             </section>
         

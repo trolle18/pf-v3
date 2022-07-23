6713fe65d1260e9d54c8483f568d4bffbbfa408d
Sofie Trolle <92292475+trolle18@users.noreply.github.com>
2022-07-23 22:22:05 +0200

    nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,20 +4,22 @@ export default function Nav() {
     return (
         <>
             <div className="nav-cntr">
-                <div className="nav-logo"> 
-                    <a href="#home">Sofie Trolle</a> 
-                </div>
-                <div className="nav-links">
-                    <a href="#about">About</a> 
-                    <a href="#work">Work</a> 
-                    <a href="#contact">Contact</a>     
-                </div>
-                {/* <div className="dropdown">
-                    <button>
-                        <IoMenuOutline/>
-                    </button>
+                <div className="nav-inner-cntr">
+                    <div className="nav-logo"> 
+                        <a href="#home">Sofie Trolle</a> 
+                    </div>
+                    <div className="nav-links">
+                        <a href="#about">About</a> 
+                        <a href="#work">Work</a> 
+                        <a href="#contact">Contact</a>     
+                    </div>
+                    {/* <div className="dropdown">
+                        <button>
+                            <IoMenuOutline/>
+                        </button>
 
-                </div> */}
+                    </div> */}
+                </div>
             </div>
         
         </>

f2134ef4762a70616cdaecd6dae815981ca00863
Sofie Trolle <92292475+trolle18@users.noreply.github.com>
2022-07-24 16:34:19 +0200

    Nav + routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { Routes, Route, Navigate } from "react-router-dom";
 import './scss/App.scss';
 import HomePage from './pages/HomePage'
+import Nav from './components/Navbar/Nav'
+import Footer from './components/Footer'
 
 
 function App() {
     return (
         <>
-            <HomePage/>       
+            <main>
+                <Nav/>  
+                <Routes>
+                    <Route path="/" element={ <HomePage/> } />
+                    <Route path="*" element={ <Navigate to="/"/> } />
+                </Routes>
+                <Footer/>
+            </main>
+                
         </>
     );
 }
--- src/index.js
+++ src/index.js
@@ -3,17 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import './scss/index.scss';
 import App from './App';
-import Nav from './components/Nav';
-import Footer from './components/Footer';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <BrowserRouter>
-            <Nav/>
             <App /> 
-            <Footer/>
         </BrowserRouter>
     </React.StrictMode>
 );

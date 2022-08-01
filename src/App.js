836d0c65c0a2a0ec3233b26b632a255ac548277f
Sofie Trolle <92292475+trolle18@users.noreply.github.com>
2022-08-01 22:50:41 +0200

    conf

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import './scss/App.scss';
 import Nav from './components/Navbar/Nav';
 import Footer from './components/Footer';
@@ -8,23 +10,46 @@ import ProjectsPage from './pages/ProjectsPage';
 import ArticlePage from "./pages/ArticlePage";
 import PostsPage from "./pages/PostsPage";
 import PostArticlePage from "./pages/PostArticlePage";
+import AuthRoutes from "./fireAuth/AuthRoutes";
 
 
 function App() {
+    const auth = getAuth();
+    const [isAuth, setIsAuth] = useState (localStorage.getItem("isAuth"));
+
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            setIsAuth(true);
+            localStorage.setItem("isAuth", true);
+        } else {
+            setIsAuth(false);
+            localStorage.removeItem("isAuth");
+        }
+    });
+
     return (
         <>
             <main>
-                <Nav/>  
-                <Routes>
-                    <Route path="/" element={ <HomePage/> } />
-                    <Route path="*" element={ <Navigate to="/"/> } />
-                    <Route path="/projects" element={ <ProjectsPage/> } />
-                    <Route path="/projects/:id" element={ <ArticlePage/> } />
-                    <Route path="/posts" element={ <PostsPage/> } />
-                    <Route path="/posts/:id" element={ <PostArticlePage/> } />                    
-                </Routes>
-                <Footer/>
-                <ScrollToTop/>
+                {isAuth ? (
+                    <>
+                        <AuthRoutes/>
+                    </>
+                ) : (
+                    <>
+                        <Nav/>  
+                        <Routes>
+                            <Route path="/" element={ <HomePage/> } />
+                            <Route path="*" element={ <Navigate to="/"/> } />
+                            <Route path="/projects" element={ <ProjectsPage/> } />
+                            <Route path="/projects/:id" element={ <ArticlePage/> } />
+                            <Route path="/posts" element={ <PostsPage/> } />
+                            <Route path="/posts/:id" element={ <PostArticlePage/> } />                   
+                        </Routes>
+                        <Footer/>
+                        <ScrollToTop/> 
+
+                    </>
+                ) }
             </main>
                 
         </>

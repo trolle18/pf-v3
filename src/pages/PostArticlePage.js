698bf159646a84e87eaf53e52ecfc4b4057df8d4
Sofie Trolle <92292475+trolle18@users.noreply.github.com>
2022-08-19 23:47:12 +0200

    article

--- src/pages/PostArticlePage.js
+++ src/pages/PostArticlePage.js
@@ -31,6 +31,12 @@ export default function PostArticlePage() {
         } 
     };
 
+    function getThisImg(post) {
+        if (post.imgs?.length >= 1) {
+            return post.imgs[1]?.imgSrc;
+        } 
+    };
+
 
     // Only render build, if the article link excists
     function getTags(post) {
@@ -55,8 +61,6 @@ export default function PostArticlePage() {
             <section className="page">
                 <div className="page-header">
                     <Link to='/posts'><BiLeftArrowAlt/> Posts </Link>
-                    
-                    {/* <h2>Article Page</h2> */}
                 </div>
 
                 <article  key={post?.id} className="article">
@@ -77,13 +81,18 @@ export default function PostArticlePage() {
 
                     <div className="article-content">
                         {/* <span dangerouslySetInnerHTML={ {__html: post.descHtml} }></span> */}
-                        <p>
-                            Mandag d. 8 august kunne jeg endelig skyde min praktikperiode i gang, 
-                            hos AKQA. Jeg skulle oprindeligt først starte d. 15 august, 
-                            men fik lov til at starte en uge før med selvstudie, for at få ugerne til at gå mest muligt op.<br/>
-                            Til mit selvstudie havde jeg forberedt lidt forskelligt materiale som jeg ville kigge på, 
-                            i form af nogle kurser på Linkedin Learning og Skillshare, samt nogle anbefalede videoer på youtube.
-                        </p> 
+                        <div className='article-text-section'>
+                            <p>
+                                Mandag d. 8 august kunne jeg endelig skyde min praktikperiode i gang, 
+                                hos AKQA. Jeg skulle oprindeligt først starte d. 15 august, 
+                                men fik lov til at starte en uge før med selvstudie, for at få ugerne til at gå mest muligt op.
+                            </p>
+                            <p>
+                                Til mit selvstudie havde jeg forberedt lidt forskelligt materiale som jeg ville kigge på, 
+                                i form af nogle kurser på Linkedin Learning og Skillshare, samt nogle anbefalede videoer på youtube.
+                            </p> 
+                        </div>
+                        
 
                         <div className='article-section'>
                             <div className='article-box'>                                            
@@ -108,39 +117,78 @@ export default function PostArticlePage() {
                                 dog virker det umiddelbart simpelt, og jeg føler at jeg let forstod de basale dele af det.
                             </p>    
                         </div>
+                        
+                        <div className='article-text-section'>
+                            <h4>Første dag på kontoret</h4> 
+                            <div className='img-cntr'>
+                                <img src={getThisImg(post)} alt=''/>
+                            </div> 
+                            <p>
+                                Mandag d. 15 august klokken 0830, skulle jeg endeligt møde op til min første dag ved AKQA, i Toldboden. 
+                                Jeg stod op klokken 0600 og følte mig frisk, nervøs og spændt på samme tid, 
+                                på trods af at jeg næsten ikke kunne sove.
+                            </p>
+                                  
+                            <p>
+                                Da jeg mødte ind blev jeg taget godt imod, og fik hilst på en masse fra kontoret, heri blandt min mentor. 
+                                Min Mentor viste mig hen til mit skrivebord og hjalp mig med at få sat computeret op, 
+                                så den havde alt jeg skulle bruge af programmer og logins. 
+                                Derefter fik vi morgenmad i køkkenet, hvor jeg fik hilst på lidt flere fra kontoret. 
+                            </p>
+                            <p>
+                                Hver mandag bliver der holdt et fællesmøde, hvor vi går igennem hvem der arbejder på hvad for ugen. 
+                                Det var spændende at se den lange liste af firmaer som der arbejdes for, 
+                                samt at høre kort om hvad nogle af projekterne går ud på.
+                            </p>
+                        </div>
+                        
+                        <div className='article-text-section'>
+                            <h4>Intro & projekt</h4>
+                            <p>
+                                Efter mødet blev jeg introduceret for to andre praktikanter jeg skal arbejde sammen med, 
+                                og det projekt vi skal arbejde på. De sidder på kontoret i København, så vores samarbejde foregår over Teams. <br/>
+                                Ud over projektet, har vi i dagligt haft flere møder, afholdt af forskellige personer fra AKQA, som fortæller om firmaet, deres kunder, hvordan de arbejder osv.
+                            </p>
+                        {/* </div> */}
 
-                        <p>
-                            Mandag d. 15 august klokken 0830, skulle jeg endeligt møde op til min første dag ved AKQA, i Toldboden. 
-                            Jeg stod op klokken 0600 og følte mig frisk, nervøs og spændt på samme tid, på trods af at jeg næsten ikke kunne sove.
-                        </p>
+                            <div className='article-section'>
+                                <div className='article-box'>                                            
+                                    <p><span>Week 1 - </span> Planning & managing</p>
+                                    <p><span>Week 2 - </span> Design & UX</p>
+                                    <p><span>Week 3 - </span> Markup</p>
+                                    <p><span>Week 4 - </span> Performance & CMS</p>
+                                    <p><span>Week 5 - </span> JS & frameworks, API dev.</p>
+                                    <p><span>Week 6 - </span> Presentation</p>
+                                </div> 
+                            
+                                <p>
+                                    Vores projekt er et 6-ugers intro-projekt som AKQA har lavet, kaldet "Flight to orbit". 
+                                    Projektet handler om et firma kaldet Space Debris Inc. (SD), i året 2122.
+                                </p>
+                                <p>
+                                    SD sørger for at hente folks skrald, og skille sig af med det, ved at smide det ud i rummet. 
+                                    Vores opgave er så, at skabe et brand til firmaet, 
+                                    samt en web-app med: en landingpage, content page, webshop og admin page.
+                                </p>
+                                <p>
+                                    I denne uge har vi været i gennem den første uge af projektet, hvor vi har sørge for planlægning og administrationen af projektet.
+                                    Jeg må indrømme at jeg nok er lidt utålmodig, og jeg glæder mig til at kunne komme i gang med at programmere, 
+                                    men det er også vigtigt at respektere at alt tager sin tid, og ikke skal hundses med.
+                                </p>
 
-                        {/* <div className='img-cntr'>
-                            <img src={post.imgs[1].imgSrc} alt={post.imgs[1].imgAlt}/>
-                        </div>         */}
-
-                        <p>
-                            Da jeg mødte ind blev jeg taget godt imod, og fik hilst på en masse fra kontoret, heri blandt min mentor. 
-                            Min Mentor viste mig hen til mit skrivebord og hjalp mig med at få sat computeret op, så den havde alt jeg skulle bruge af programmer og logins. 
-                            Derefter fik vi morgenmad i køkkenet, hvor jeg fik hilst på lidt flere fra kontoret. 
-                        </p>
-                        <p>
-                            Hver mandag bliver der holdt et fællesmøde, hvor vi går igennem hvem der arbejder på hvad for ugen. 
-                            Det var spændende at se den lange liste af firmaer som der arbejdes for, samt at høre kort om hvad nogle af projekterne går ud på.<br/><br/>
-                        </p>
-
-                        <h4>Projekt</h4>
-                        <p>
-                            Efter mødet blev jeg introduceret for to andre praktikanter jeg skal arbejde sammen med, og det projekt vi skal arbejde på. 
-                            De sidder på kontoret i København, så vores samarbejde foregår over Teams. <br/><br/>
-                            Vores projekt er et intro-projekt som AKQA har lavet, kaldet "Flight to orbit". Projektet handler om et firma kaldet Space Debris Inc. (SD), i året 2122. <br/>
-                            SD sørger for at hente folks skrald, og skille sig af med det, ved at smide det ud i rummet. Vores opgave er så, at skabe et brand til firmaet, 
-                            samt en web-app med: en landingpage, content page, webshop og admin page. <br/>
-                            Vi har fået rimeligt frie tøjler, og har lov til at tage både kreativ og humoristisk frihed, og indtil videre virker det rigtigt sjovt.
-                        </p>
-                        <p>
-                            Jeg er spændt på at se hvad vi kan få kreeret. De to andre praktikanter, som jeg arbejder med virker begge super dygtige. 
-                            Den ene er en multimediedesigner, specialiserende i frontend ligesom mig, og den anden er en datamatiker. 
-                        </p>
+                            </div>
+                        
+                        {/* <div className='article-text-section'> */}
+                            <p>
+                                Vi har fået rimeligt frie tøjler, og har lov til at tage både kreativ og humoristisk frihed, 
+                                og indtil videre virker det rigtigt sjovt.
+                            </p>
+                            <p>
+                                Jeg er spændt på at se hvad vi kan få kreeret. De to andre praktikanter, som jeg arbejder med virker begge super dygtige. 
+                                Den ene er en multimediedesigner, specialiserende i frontend ligesom mig, og den anden er en datamatiker. 
+                            </p>    
+                        </div>
+                        
 
                     </div>
                    

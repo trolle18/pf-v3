0347ba6203a8599fc1eec40d23d6eca866bdf2b3
Sofie Trolle <92292475+trolle18@users.noreply.github.com>
2022-09-02 16:46:31 +0200

    edits + content

--- src/pages/PostArticlePage.js
+++ src/pages/PostArticlePage.js
@@ -58,6 +58,77 @@ export default function PostArticlePage() {
                     </div>
 
                     <div className="article-content" dangerouslySetInnerHTML={ {__html: post.descHtml} }></div>    
+
+                    {/* <div class='article-content'>
+                        <div class='article-text-section'>
+                            <p>Mandag d. 22 er starten på min 3. uge i praktikken, og jeg er så småt begyndt at føle jeg er faldet til, selvom jeg kan mærke jeg stadigt er meget genert blandt alle de nye mennesker, heldigvis er alle super søde, nede på jorden og gode til at joke med hinanden. Alt i alt er det et super dejligt kontor jeg er havnet i!</p>
+                            <p>
+                                I denne uge kunne min projektgruppe og jeg endeligt gå i gang med design-delen af projektet Space Debris Inc. Vores forrige uge var meget mere løs og en general into, i denne uge har vi bestemt haft mere at se til, i form af skitser, mockups, moodboards, wireframes, mockups og logodesign.
+                                Vi har fået en ny "uge-ansvarlig", som i denne uge var Nicklas. Vi har en ny hver uge for at møde og arbejde med forskellige "team-leads", sidste uges uge-ansvarlige var Simon H.
+                            </p>
+                        
+                            </div>
+
+                        <div class='article-text-section'>
+                            <h4>Uge 3</h4>
+                            <p>
+                                I denne uge kunne min projektgruppe og jeg endeligt gå i gang med design-delen af projektet Space Debris Inc. Vores forrige uge var meget mere løs og en general into, i denne uge har vi bestemt haft mere at se til, i form af skitser, mockups, moodboards, wireframes, mockups og logodesign.
+                                Vi har fået en ny "uge-ansvarlig", som i denne uge var Nicklas. Vi har en ny hver uge for at møde og arbejde med forskellige "team-leads", sidste uges uge-ansvarlige var Simon H.
+                            </p>
+                            <p>
+                                Mandagen havde mig gruppe og jeg et møde sammen med vores uge-ansvarlige, Nicklas. Her snakkede vi om hvad vi havde lavet indtil videre, hvordan det gik, hvad vi skal i denne uge osv.
+                            </p>
+                            <p>
+                                Nicklas foreslog at ham og jeg kunne tage til København og se kontoret der ovre, samt give mig muligheden for at møde de andre praktikanter som jeg har arbejdet sammen med. Hos AKQA Denmark er det nemt at kunne rejse mellem de to kontorer for at arbejde, hvilket jeg synes er super fedt, da jeg kan forestille mig at det fremmer samarbejdet mellem kontorene en del.
+                            </p>
+
+                            <p>
+                                Nicklas fik arengeret turen til København, og tidligt onsdag morgen tog jeg afsted med toget fra Aarhus Hovedbanegård, med en dejlig plads i 1' klasse. Nicklas hoppede med på toget i Frederecia, og så gik turen ellers mod København. det var super fedt at få lov til at hilse på de andre praktikanter, men også at se kontoret i København, og bruge lidt tid der ovre.
+                            </p>
+                        </div>
+
+                        <div class='article-text-section'>
+                            <h4>Amsterdam</h4>
+                            <div class='img-cntr'>
+                                <img src='/data/media/posts/amsterdamhuse.jpg' alt='amsterdam huse'/>
+                            </div>
+                            <p>
+                                Uge 3 sluttede med en tur til Amsterdam, fra fredag d. 26 til søndag d. 28. Hvert år tager AKQA på en firmarejse i en weekend, sidste år var de f.eks. i Berlin. Jeg var så heldig at få lov til at komme med! De sørgede for fly og hotel, samt aftensmad lørdag aften.
+                            </p>
+                            <p>
+                                Vi landede med flyet i Amsterdam omkring klokken 11, og var fremme ved hotellet ca. en time senere. Jeg havde forventet at jeg skulle dele værelse med nogen, men nej-nej, jeg fik mit eget med en stor lækker seng og lækkert badeværelse! 
+                            </p>
+                            <p>
+                                Fredagen blev hovedsagligt brugt på at gå lidt rundt i området og se lidt af byen, nogen havde planer, selv skulle jeg i et escape room med nogle af de andre. Om aftenen var vi inviteret til fest ved noget der hed Waterkant, hvor der var fri bar til alle fra AKQA. 
+                            </p>
+                            <p>
+                                Lørdag var endnu en rolig dag, den lignede meget dagen før. Om eftermiddagen tog jeg på museeum med en anden fra kontoret, hvilket var super fedt. Lørdag aften var der en firma-event i SkyLounge Amsterdam, det havde en fantastisk udsigt over Amsterdam! Det var meget fancy og exclusivt, så jeg følte mig en smule under-dressed i mine jeans, t-shirt og åbne skovmands-skjorte... Heldigvis så jeg hurtigt mange i lignende tøj, så helt ved siden af var jeg ikke!
+                            </p>
+                            <div class='img-cntr'>
+                                <img src='/data/media/posts/img_20220827_191229e.jpg' alt='skylounge amsterdam'/>
+                            </div>
+                        </div>
+
+                            
+
+                            <div class='article-text-section'>
+                                <h4>Uge 4</h4>
+                                <p>
+                                    Uge 4 startede vi op igen klar og friske (nogenlunde da) mandag morgen, hvor vi nu kunne få lov til at gå i gang med at kigge på noget programmering! Denne uges uge-ansvarlige er Simon (En anden Simon end den tidligere uge-ansvarlige).
+                                </p>
+                                <p>
+                                    Mandag gav Simon et hurtigt crash-course i Next bygget med TypeScript, da det er dét vi skal bruge til at lave vores projekt. Der var jeg glad for at jeg havde kigget på det i forvejen, for selvom det ligner React ret meget - da det er bygget på det, er der selvfølgeligt stadigt nogle ting som skal gøres anderledes.
+                                </p>
+                                <p>
+                                    Tirisdag gav Simon os et crash-course i Chakra UI, som til gengæld er noget helt nyt for mig. Det er et library som man kan bruge til React, til at style componenter og moduler nemt og hurtigt. <br/>
+                                    Onsdag kiggede på vi data-flow i Next, og hvordan man bedst binder det sammen med en REST API, og for det renderet bedst muligt.
+                                </p>
+                                <p>
+                                    Min multimediedesigner makker i gruppen havde haft mere besvær med Chakra UI end hvad var nyttigt, og havde desværre ikke haft oplevelsen af at det skulle gøre det nemmere og hurtigere at bygge web apps. Jeg har heldigvis været heldig, og siddet lige bag ved Simon, og derved haft let ved at søge hjælp. <br/>
+                                    Vi har derfor spurgt om lov til at få indkorporeret lidt sass i projektet, til at lette frustrationerne og få styr på stylingen.
+                                </p>
+                            </div>
+                    </div> */}
                                                 
                 </article>
 

00c159b5b703ccc84d3c5109787fed631aa79d0f
Sofie Trolle <92292475+trolle18@users.noreply.github.com>
2022-10-16 22:32:47 +0200

    hide nav on scroll

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,20 @@
 import '../scss/Nav.scss'; 
 
-
 export default function Nav() {
+    var prevScrollPos = window.pageYOffset;
+    window.onscroll = function() {
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollPos > currentScrollPos) {
+            document.getElementById("navBar").style.top = "0"
+        } else {
+            document.getElementById("navBar").style.top = "-3em"
+        }
+        prevScrollPos = currentScrollPos
+    };
     
     return (
         <>
-            <div className="nav-cntr">
+            <div className="nav-cntr" id="navBar">
                 <div className="nav-inner-cntr">
 
                     <div className="nav-logo"> 

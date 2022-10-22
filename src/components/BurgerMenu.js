3f0ffe9018a388cdfd7b1c9a47c48d9dcec5e80c
Sofie Trolle <92292475+trolle18@users.noreply.github.com>
2022-10-22 20:41:27 +0200

    burger menu - hide dropdown on scroll

---
+++ src/components/BurgerMenu.js
@@ -0,0 +1,61 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import '../scss/Nav.scss'; 
+import { useEffect, useState } from 'react';
+
+const BurgerMenu = () => {
+
+  const [show, setShow] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  // HIDE DROPDOWN ON SCROLL
+  const controlShow = () => {
+    if (typeof window !== 'undefined') { 
+      if (window.scrollY > lastScrollY) { // if scroll down hide
+        setShow(false); 
+      } else { setShow(true) } // if scroll up show 
+      setLastScrollY(window.scrollY); // remember current page location to use in the next move
+    }
+  };
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll', controlShow);
+      return () => { // cleanup function
+        window.removeEventListener('scroll', controlShow);
+      };
+    }
+  }, [lastScrollY]);
+
+
+  // OPEN BURGER MENU
+  const openMenu = () => {
+    const button = document.getElementById("menu-btn");
+    const y = document.getElementById("dropdown");
+    button.classList.toggle("change");
+    if (y.classList.contains("hidden")) {
+      y.classList.remove("hidden");
+      y.classList.add("show");
+    } else {
+      y.classList.remove("show");
+      y.classList.add("hidden");
+    }
+  }
+
+  return (
+    <>
+      <button onClick={openMenu} className="nav-mob-links__menu-btn" id="menu-btn" >
+        <svg viewBox="0 0 10 8">
+          <path d="M1 1h8M1 "/>
+          <path d="M1 4h 8M1"/>
+          <path d="M1 7h8"/>
+        </svg>
+      </button>
+      <div className={`nav-mob-links__dropdown hidden ${show && ''}`} id="dropdown" >
+        <a href="#about"> About </a> 
+        <a href="#work"> Work </a> 
+        <a href="#contact"> Contact </a> 
+      </div>
+    </>
+  );
+};
+
+export default BurgerMenu;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,25 +1,6 @@
 import '../scss/Nav.scss'; 
 import React, { useState, useEffect } from 'react';
-
-    // // Intersection observer
-    // let cardObserverOptions = {
-    //     threshold: 0.25
-    // };
-    // var cardObserver = new IntersectionObserver(cardObserverCallback, cardObserverOptions);
-    // function cardObserverCallback(entries, observer) {
-    //     entries.forEach(entry => {
-    //         entry.target.classList.toggle("show", entry.isIntersecting)
-    //         if (entry.isIntersecting) {
-    //             observer.unobserve(entry.target)
-    //         }
-    //     });
-    // };
-    // let cardTarget = '.card';
-    // document.querySelectorAll(cardTarget).forEach((i) => {
-    //     if (i) {
-    //         cardObserver.observe(i);
-    //     }
-    // });
+import BurgerMenu from './BurgerMenu';
 
 const Nav = () => {
   const [show, setShow] = useState(true);
@@ -44,18 +25,6 @@ const Nav = () => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [lastScrollY]);
 
-  const openMenu = () => {
-    const button = document.getElementById("menu-btn");
-    const y = document.getElementById("dropdown");
-    button.classList.toggle("change");
-    if (y.classList.contains("hidden")) {
-      y.classList.remove("hidden");
-      y.classList.add("show");
-    } else {
-      y.classList.remove("show");
-      y.classList.add("hidden");
-    }
-  }
 
   return (
         <nav className={`active ${show && ''}`} id="nav-cntr">
@@ -70,15 +39,7 @@ const Nav = () => {
                   </div>
 
                   <div className="nav-mob-links">
-                    <button onClick={openMenu} className="nav-mob-links__menu-btn" id="menu-btn" >
-                      <svg viewBox="0 0 10 8"><path d="M1 1h8M1 4h 8M1 7h8"/></svg>
-                    </button>
-                    <div className="nav-mob-links__dropdown hidden" id="dropdown" >
-                      <a href="#about"> About </a> 
-                      <a href="#work"> Work </a> 
-                      <a href="#contact"> Contact </a> 
-                    </div>
-                    
+                    <BurgerMenu/>                    
                   </div>       
                                
              </div>

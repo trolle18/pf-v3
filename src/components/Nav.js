c0d5fd49a5cc10ed19beb0f00fe28c311f7a591a
Sofie Trolle <92292475+trolle18@users.noreply.github.com>
2022-10-22 15:19:14 +0200

    nav scroll function

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,35 +1,50 @@
 import '../scss/Nav.scss'; 
 
-export default function Nav() {
-    var prevScrollPos = window.pageYOffset;
-    window.onscroll = function() {
-        var currentScrollPos = window.pageYOffset;
-        if (prevScrollPos > currentScrollPos) {
-            document.getElementById("navBar").style.top = "0"
-        } else {
-            document.getElementById("navBar").style.top = "-3em"
-        }
-        prevScrollPos = currentScrollPos
-    };
-    
-    return (
-        <>
-        <nav className="nav-cntr" id="navBar">
-            <div className="nav-inner-cntr">
+import React, { useState, useEffect } from 'react';
 
-                <div className="nav-logo"> 
-                    <a href="/">Sofie Trolle</a> 
-                </div>
-                <div className="nav-links">
-                    <a href="#about"> About </a> 
-                    <a href="#work"> Work </a> 
-                    <a href="#contact"> Contact </a>                         
-                </div>
-                
-            </div>
+const Nav = () => {
+  const [show, setShow] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  const controlNavbar = () => {
+    if (typeof window !== 'undefined') { 
+      if (window.scrollY > lastScrollY) { // if scroll down hide the navbar
+        setShow(false); 
+      } else { // if scroll up show the navbar
+        setShow(true);  
+      }
+
+      // remember current page location to use in the next move
+      setLastScrollY(window.scrollY); 
+    }
+  };
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll', controlNavbar);
+
+      // cleanup function
+      return () => {
+        window.removeEventListener('scroll', controlNavbar);
+      };
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [lastScrollY]);
+
+  return (
+        <nav className={`active ${show && ''}`} id="nav-cntr">
+            <div className="nav-inner-cntr">
+                 <div className="nav-logo"> 
+                     <a href="/">Sofie Trolle</a> 
+                 </div>
+                 <div className="nav-links">
+                     <a href="#about"> About </a> 
+                     <a href="#work"> Work </a> 
+                     <a href="#contact"> Contact </a>                         
+                 </div>                
+             </div>
         </nav>
-        </>
-    )
+  );
 };
 
-
+export default Nav;

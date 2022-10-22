701de3dc89d014c59b9bfaa3ca5ba6967d90a8ea
Sofie Trolle <92292475+trolle18@users.noreply.github.com>
2022-10-22 19:35:46 +0200

    ScrollToTop scss break-up + style edits

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
-import { IoIosArrowUp } from "react-icons/io"
-
+import { IoIosArrowUp } from "react-icons/io";
+import "../scss/ScrollToTop.scss";
 
 export default function ScrollToTop() {
 

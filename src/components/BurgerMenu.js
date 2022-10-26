b1edb5728afe984d657d2b969c032cb321ab4126
Sofie Trolle <92292475+trolle18@users.noreply.github.com>
2022-10-26 17:18:44 +0200

    burger nav show true/false

--- src/components/BurgerMenu.js
+++ src/components/BurgerMenu.js
@@ -45,7 +45,7 @@ const BurgerMenu = () => {
       <button onClick={openMenu} className="nav-mob-links__menu-btn" id="menu-btn" >
         <svg viewBox="0 0 10 8"> <path d="M1 1h8M1"/> <path d="M1 4h 8M1"/> <path d="M1 7h8"/> </svg>
       </button>
-      <div className={`nav-mob-links__dropdown hidden ${show && ''}`} id="dropdown" >
+      <div className={`nav-mob-links__dropdown hidden ${show && ""}`} id="dropdown" >
         <a href="#about"> About </a> 
         <a href="#work"> Work </a> 
         <a href="#contact"> Contact </a> 

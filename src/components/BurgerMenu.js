f07ffc99e0c9781b8235e4d6c88e2eb4370c999b
Sofie Trolle <92292475+trolle18@users.noreply.github.com>
2022-11-09 17:47:02 +0100

    mobile menu

--- src/components/BurgerMenu.js
+++ src/components/BurgerMenu.js
@@ -44,11 +44,6 @@ export default function BurgerMenu({data}) {
         <svg viewBox="0 0 10 8"><path d="M1 1h8M1 4h 8M1 7h8"/></svg>
       </button>
       <div className={`nav-mob-links__dropdown hidden ${show && ""}`} id="dropdown" >
-        {/* {data.navLinks.map((navLink) => (
-          <a key={navLink.id} href={navLink.link}>
-            {navLink.linkTxt}
-          </a>
-        ))}     */}
         <a href="#about"> About </a> 
         <a href="#work"> Work </a> 
         <a href="#contact"> Contact </a> 

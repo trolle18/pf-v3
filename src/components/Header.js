944d89886d8faf75ba07cd6e37b2f9f6c4407c41
Sofie Trolle <92292475+trolle18@users.noreply.github.com>
2023-01-12 22:03:49 +0100

    formatting

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,15 +4,15 @@ import { container, element } from "../utils/framerMotion";
 
 export default function Header() {
     const [sectionData, setSectionData] = useState([]);
-    
+
     // Fetch data from JSON
     useEffect(() => {
         async function getData() {
             const response = await fetch("/data/components/homepageData.json");
             const data = await response.json();
-            setSectionData(data);             
-        }       
-        getData();        
+            setSectionData(data);
+        }
+        getData();
     }, []);
 
 
@@ -23,7 +23,7 @@ export default function Header() {
                 return <h1 variants={element}>{data.text}</h1>
             } else {
                 return <span>{data.text}</span>
-                
+
             }
         }
     }
@@ -31,27 +31,27 @@ export default function Header() {
 
     return (
         <>
-        {sectionData.map((data) => (
-            <section key={data.id} className="header-cntr sec">
-                 {data.header.map((data) => (
-                <motion.div 
-                key={data.id}
-                className="header-cntr__txt"
-                variants={container}
-                initial="hidden"
-                whileInView="show"
-                viewport={{once: true}}
-                >
-                    {data?.text.map((data) => (
-                        <motion.div variants={element} key={data.id}>
-                            {TextType(data)}
-                        </motion.div>
-                    
-                    ))}   
-                </motion.div>
-                 ))}  
-            </section>
-        ))}   
+            {sectionData.map((data) => (
+                <section key={data.id} className="header-cntr sec">
+                    {data.header.map((data) => (
+                    <motion.div
+                    key={data.id}
+                    className="header-cntr__txt"
+                    variants={container}
+                    initial="hidden"
+                    whileInView="show"
+                    viewport={{once: true}}
+                    >
+                        {data?.text.map((data) => (
+                            <motion.div variants={element} key={data.id}>
+                                {TextType(data)}
+                            </motion.div>
+
+                        ))}
+                    </motion.div>
+                    ))}
+                </section>
+            ))}
         </>
     )
 };

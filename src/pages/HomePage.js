3fc394cad9fcb655fdac15da88547cd192a7e884
Sofie Trolle <92292475+trolle18@users.noreply.github.com>
2023-01-15 20:38:19 +0100

    hero - renamed

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import Header from '../components/Header';
+import Hero from '../components/Hero';
 import AboutSection from '../components/sections/AboutSection';
 import ContactSection from '../components/sections/ContactSection';
 import WorkSection from '../components/sections/WorkSection';
@@ -21,7 +21,7 @@ export default function HomePage() {
         <>
             {pageData?.map((data) => (
                 <section className="page" key={data?.id}>
-                    <Header data={data}/>
+                    <Hero data={data}/>
                     <AboutSection data={data}/>
                     <WorkSection data={data} />
                     <ContactSection data={data}/>
